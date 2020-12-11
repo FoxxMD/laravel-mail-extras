ebc21eaeaa70743697899f1a7f524b7a2016c6d9
FoxxMD <FoxxMD@users.noreply.github.com>
2020-12-11 13:24:00 -0500

    Update to work with laravel 5.4

--- src/LaravelMailExtrasServiceProvider.php
+++ src/LaravelMailExtrasServiceProvider.php
@@ -9,45 +9,32 @@ use Illuminate\Mail\MailServiceProvider;
 
 class LaravelMailExtrasServiceProvider extends MailServiceProvider {
 
-	public function boot()
-	{
-		// nothing to do here!
-	}
-
 	/**
-	 * Register the service provider.
+	 * Register the Illuminate mailer instance.
 	 *
 	 * @return void
 	 */
-	public function register()
+	protected function registerIlluminateMailer()
 	{
-		$this->registerSwiftMailer();
-
 		$this->app->singleton('mailer', function ($app) {
+			$config = $app->make('config')->get('mail');
+
 			// Once we have create the mailer instance, we will set a container instance
 			// on the mailer. This allows us to resolve mailer classes via containers
 			// for maximum testability on said classes instead of passing Closures.
-
-			// only change here is the use of FoxxMD Mailer instead of Illuminate Mailer
 			$mailer = new Mailer(
 				$app['view'], $app['swift.mailer'], $app['events'], $app['config']
 			);
 
-			$this->setMailerDependencies($mailer, $app);
-
-			// If a "from" address is set, we will set it on the mailer so that all mail
-			// messages sent by the applications will utilize the same "from" address
-			// on each one, which makes the developer's life a lot more convenient.
-			$from = $app['config']['mail.from'];
-
-			if (is_array($from) && isset($from['address'])) {
-				$mailer->alwaysFrom($from['address'], $from['name']);
+			if ($app->bound('queue')) {
+				$mailer->setQueue($app['queue']);
 			}
 
-			$to = $app['config']['mail.to'];
-
-			if (is_array($to) && isset($to['address'])) {
-				$mailer->alwaysTo($to['address'], $to['name']);
+			// Next we will set all of the global addresses on this mailer, which allows
+			// for easy unification of all "from" addresses as well as easy debugging
+			// of sent messages since they get be sent into a single email address.
+			foreach (['from', 'reply_to', 'to'] as $type) {
+				$this->setGlobalAddress($mailer, $config, $type);
 			}
 
 			return $mailer;
